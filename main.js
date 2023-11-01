eb55673a5c5b396014107ebf2e38f07d0bde3aac
beveridgej <beve0016@algonquinlive.com>
2023-11-01 17:51:07 -0400

    adds overlay, pokemon bootstrap grid, side nav

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+
+// slideing nav function 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
