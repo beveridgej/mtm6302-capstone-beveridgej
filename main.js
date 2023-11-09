9534dd1e9e99e8b9cfe4aa04c9f8b5a003c904ff
beveridgej <beve0016@algonquinlive.com>
2023-11-08 19:30:20 -0500

    adds comments to all files

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
+
+// Gets the side navigation and main content elements
 const mySideNav = document.getElementById("mySideNav");
 const main = document.getElementById("main");
 
-// Open navigation
+// Open sidenav function
 function openNav() {
-    mySideNav.style.width = "250px";
-    main.style.marginLeft = "250px";
+    mySideNav.style.width = "250px"; // Sets the width of the sidenav to 250px
+    main.style.marginLeft = "250px"; // Moves the main content to the right by 250px
 }
 
-// Close navigation
+// Close sidenav function
 function closeNav() {
-    mySideNav.style.width = "0";
-    main.style.marginLeft= "0";
+    mySideNav.style.width = "0"; // Sets the width of the side navigation to 0
+    main.style.marginLeft= "0"; // Move the main content back to the left
 }
 
-// Add event listeners
-document.addEventListener('openNav', openNav);
-document.addEventListener('closeNav', closeNav);
+// Adds event listeners
+document.addEventListener('openNav', openNav); // Adds event listeners for opening the sidenav
+document.addEventListener('closeNav', closeNav); // Adds event listeners for opening the sidenav
 
-const overlay = document.getElementById('overlay'); // 
-const button = document.querySelector('.button'); // 
+const overlay = document.getElementById('overlay');  // Gets the overlay and button elements
+const button = document.querySelector('.button'); 
 const btnclose = document.getElementById('btnclose')
-// Initially hide the overlay
 
 
-button.addEventListener('click', function() {
-    // If the overlay is hidden, show it. Otherwise, hide it.
-    if (overlay.style.display === 'none') {
+
+button.addEventListener('click', function() { // Add an event listener to the button for toggling the overlay
+    
+    if (overlay.style.display === 'none') {  // If the overlay is hidden, show it. Otherwise, hide it.
         overlay.style.display = 'block';
     } else {
         overlay.style.display = 'none';
@@ -33,15 +35,15 @@ button.addEventListener('click', function() {
 });
 
 
-btnclose.addEventListener('click', function() {
-  overlay.style.display = 'none';
+btnclose.addEventListener('click', function() { // Add an event listener to the close button for hiding the overlay
+    overlay.style.display = 'none';
 }); 
 
 
-const sidebar = document.getElementById('mySideNav');
+const sidebar = document.getElementById('mySideNav'); // Gets the sidebar and toggle button elements
 const toggleButton = document.getElementById('toggleButton');
 
-// Create the image element
+// Creates the image element with a grid in the sidenav
 let img = document.createElement('img');
 img.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
 sidebar.style.display = 'grid';
@@ -50,13 +52,13 @@ sidebar.style.gridAutoRows = 'auto';
 sidebar.style.justifyItems = 'center';
 
 
-toggleButton.addEventListener('click', function() {
+toggleButton.addEventListener('click', function() { // Adds an event listener to the toggle button for adding/removing the image
     
-    if (sidebar.contains(img)) {
-        // If it is, remove it
-        sidebar.removeChild(img);
+    if (sidebar.contains(img)) {  // If the image is in the sidebar, remove it
+        
+        sidebar.removeChild(img);  
     } else {
-        // If it's not, add it
-        sidebar.appendChild(img);
+        
+        sidebar.appendChild(img); // If the image is not in the sidebar, add it
     }
 });
